9d5df37c7cb9cb92e72eb3f58b02ba41b0acb5ff
nakanaide1024 <71699010+nakanaide1024@users.noreply.github.com>
2021-06-02 18:52:19 +0900

    マイページの呼び出しのメソッド作成

--- app/Http/Controllers/EpisodeController.php
+++ app/Http/Controllers/EpisodeController.php
@@ -3,11 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class EpisodeController extends Controller
 {
-    //top.blade.phpを呼び出しトップページを出力する
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * top.blade.phpを呼び出しトップページを出力する
+     * @param void
+     * @return view
+     */
     public function index(){
         return view('first');
     }
+
+    /**
+     * マイページを呼び出す
+     * @param void
+     * @return view
+     */
+
+     public function show(){
+         $auth = Auth::user();
+         return view('user.mypage', ['auth' => $auth]);
+     }
 }
