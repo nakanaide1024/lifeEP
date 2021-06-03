d391f11285db008dbed88471510fc06e2cf008c0
nakanaide1024 <71699010+nakanaide1024@users.noreply.github.com>
2021-06-03 10:45:38 +0900

    カテゴリーの見出し編集

--- resources/views/user/mypage.blade.php
+++ resources/views/user/mypage.blade.php
@@ -5,8 +5,13 @@
       <div class="col-lg-6 display-2">
         {{ $auth->nickname }}
       </div>
-      <div class="col-sm-3">エピソードを追加する</div>
-      <div class="col-sm-3">共有する</div>
+      <div class="col-lg-3" style="line-height: 4rem;">エピソードを追加する</div>
+      <div class="col-lg-3" style="line-height: 4rem;">共有する</div>
+    </div>
+    <d class="row" style="line-height: 4rem; margin-top: 2rem;">
+      <div class="col-4"></div>
+      <div class="col-4 border-bottom text-center align-bottom">category</div>
+      <div class="col-4"></div>
     </div>
   </div>
 @endsection
