f7dd50e13dacfe846d49fa085c4cdc63f1d4ae8b
nakanaide1024 <71699010+nakanaide1024@users.noreply.github.com>
2021-06-05 10:55:41 +0900

    footerの記述変更

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,5 +1,3 @@
-<footer class="footer bg-info" style="margin-top: 8%;">
-    <div class="container text-center">
-      <span class="text-light">©︎上りだけ坂 2021</span>
-    </div>
-</footer>
+<div class="container text-center">
+  <span class="text-light">©︎上りだけ坂 2021</span>
+</div>
