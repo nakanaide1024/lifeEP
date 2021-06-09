f18f5a3004dc21af4a58995698dedf2f2865af84
nakanaide1024 <71699010+nakanaide1024@users.noreply.github.com>
2021-06-09 20:46:48 +0900

    ルーティングのコメント追加

--- routes/web.php
+++ routes/web.php
@@ -17,19 +17,35 @@ use \App\Http\Controllers\EpisodeController;
 |
 */
 
+//トップ画面の表示
 Route::get('/',[EpisodeController::class,'index'])->name('top');
+//マイページ画面の表示
 Route::get('/mypage/{id}',[EpisodeController::class, 'show'])->name('show');
 
+
+//エピソード投稿画面の表示
 Route::get('/new',[EpisodeController::class, 'new'])->name('new');
+//エピソード登録
 Route::post('/create',[EpisodeController::class, 'exeStore'])->name('create');
+//エピソード編集画面の表示
 Route::get('/edit/{id}',[EpisodeController::class, 'edit'])->name('edit');
+//エピソード編集
 Route::post('/update',[EpisodeController::class, 'exeUpdate'])->name('update');
+//エピソード削除
 Route::post('/delete/{id}',[EpisodeController::class, 'exeDelete'])->name('delete');
 
+
+//ログイン画面の表示
 Route::get('login', [LoginController::class,'showLoginForm'])->name('login');
+//ゲストログイン
 Route::get('guest', [LoginController::class,'guestLogin'])->name('guest');
+//ログイン
 Route::post('login', [LoginController::class,'login']);
+//ログアウト
 Route::post('logout', [LoginController::class,'logout'])->name('logout');
 
+
+//新規登録画面の表示
 Route::get('register', [RegisterController::class,'showRegistrationForm'])->name('register');
+//新規登録
 Route::post('register', [RegisterController::class,'register']);
